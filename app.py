be38c4870066cd4b799d0a0ce0e8617c8b4f27c3
mrprokoala <zunigaruizb@gmail.com>
2019-05-06 14:08:13 +0800

    fix

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import pygame
-# INITIAL STATE (DIRECTION IS IN DEGREES WITH 90 FACING UP)
-state = {'score': 0, 'time': 0, 'fuel': 1000, 'altitude': 400,
-         'horizontal_speed': 400, 'vertical_speed': 0, 'direction': 90}
+# INITIAL STATE (POSITION IS MEASURED FROM THE TOP LEFT CORNER, DIRECTION IS IN DEGREES WITH 90 FACING UP)
+state = {'pos_x': 10, 'pos_y': 10, 'score': 0, 'time': 0, 'fuel': 1000,
+         'altitude': 400, 'vertical_speed': 0, 'boost_duration': 0}
 
 
-def accelerate_vertical:
+def accelerate_vertical():
     # PHYSICS
+
+
+def hacer_cosas():
+    # BOOST
 
 
 pygame.init()
@@ -25,12 +29,8 @@ while(True):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        if event.type == pygame.KEYDOWN
-        if event.key == pygame.K_LEFT:
-                # ROTATE LEFT
-            if event.key == pygame.K_RIGHT:
-                # ROTATE LEFT
+        if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                # ROTATE LEFT
-            if event.key == pygame.K_s:
-                # SPEED UP THE GAME
+                hacer_cosas()
+        else:
+            # RESET DURATION
