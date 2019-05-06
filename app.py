3187edf5aa9cdee3091ad8001a4cbd4c47b3b4b0
19davidw <46531148+19davidw@users.noreply.github.com>
2019-05-06 14:06:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,6 @@ state = {'score': 0, 'time': 0, 'fuel': 1000, 'altitude': 400,
          'horizontal_speed': 400, 'vertical_speed': 0, 'direction': 90}
 
 
-def accelerate_vertical:
-    # PHYSICS
-
-
 pygame.init()
 
 size = width, height = 500, 500
@@ -34,3 +30,48 @@ while(True):
                 # ROTATE LEFT
             if event.key == pygame.K_s:
                 # SPEED UP THE GAME
+class Vector:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def setPos(self,x,y):
+        self.x = x
+        self.y = y
+    def mag(self):
+        return math.hypot(self.x,self.y)
+    def getPos(self):
+        return [self.x,self.y]
+    def norm(self):
+        ans = [self.x/self.getMng(),self.y/self.getMng()]
+        ansV = Vector(ans[0],ans[1])
+        return ansV
+    def addV(self, v):
+        self.x+=v.getPos()[0]
+        self.y+=v.getPos()[1]
+    def subV(self, v):
+        self.x-=v.getPos()[0]
+        self.y-=v.getPos()[1]
+    def getAngle(self):
+        if(self.x==0):
+            if(self.y>0):
+                return math.pi/2
+            else:
+                return math.pi*3/2
+        ans = math.atan(self.y/self.x)
+        if (self.x<0):
+            return ans+math.pi
+        return ans
+    def scalarMult(self, s):
+        self.x *= s
+        self.y *= s
+#Physics
+FUEL_CONSTANT = 1;
+GRAVITY_CONSTANT = -1;
+RESISTANCE_CONSTANT = -0.01;
+DIRECTION = new Vector(0,1);
+
+def update(state, fuel):
+    position = new Vector(init_x, init_y)
+    time = 0;
+    state[3]+=state[5]
+    state[5]+=fuel*FUEL_CONSTANT+GRAVITY_CONSTANT;
