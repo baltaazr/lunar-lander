9b828bfbd514955ebdbfcc7f7e3b9ab5971d248e
freshkoala <zunigaruizb@gmail.com>
2019-05-11 16:31:04 +0800

    DONE

--- app.py
+++ app.py
@@ -1,94 +1,81 @@
 import pygame
-# INITIAL STATE (POSITION IS MEASURED FROM THE TOP LEFT CORNER, DIRECTION IS IN DEGREES WITH 90 FACING UP)
-state = {'pos_x': 10, 'pos_y': 10, 'score': 0, 'time': 0, 'fuel': 1000,
-         'altitude': 400, 'vertical_speed': 0, 'boost_duration': 0}
+import math
+import sys
+# INITIAL STATE
+state = {'altitude': 490, 'fuel': 1000,  'speed': 0, 'timer': 0}
+
+step = 0.001
 
 
-def accelerate_vertical():
-    # PHYSICS
+def gravity():
+    # A(t) = 9.8
+    state['speed'] -= 9.8 * step
+    state['altitude'] += state['speed'] * step
 
 
-def hacer_cosas():
-    # BOOST
+def boost():
+    # A(t) = 9.8 - 50 = 40.2
+    state['speed'] += 40.2 * step
+    state['altitude'] += state['speed'] * step
+    state['fuel'] -= 0.1
+
+
+def get_triangle_cords(x, y, r=10):
+    return [(x, y - r), (x + r*math.cos(math.pi*(5/4)), y - r*math.sin(math.pi*(5/4))), (x + r*math.cos(math.pi*(7/4)), y - r*math.sin(math.pi*(7/4)))]
 
 
 pygame.init()
 
-size = width, height = 500, 500
+pygame.font.init()
+
+myfont = pygame.font.SysFont('Arial', 15)
+
+size = width, height = 200, 500
 black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
 
 while(True):
     # RUN GRAPHICS USING STATE
+    screen.fill(black)
 
-    # UPDATE STATE DEPENDING ON PHYISICS
-    accelerate_vertical()
+    altitude_text = myfont.render(
+        'altitude: ' + str(round(state['altitude'])), False, (255, 255, 255))
+
+    fuel_text = myfont.render(
+        'fuel: ' + str(round(state['fuel'])), False, (255, 255, 255))
+
+    speed_text = myfont.render(
+        'speed: ' + str(round(state['speed'])), False, (255, 255, 255))
+
+    screen.blit(altitude_text, (0, 0))
+    screen.blit(fuel_text, (0, 20))
+    screen.blit(speed_text, (0, 40))
+
+    pygame.draw.line(screen, (255, 255, 255), (0, 490), (500, 490))
+
+    pygame.draw.polygon(screen, (255, 255, 255), [(round(triangle[0]), round(triangle[1])) for triangle in get_triangle_cords(
+        100, 500 - state['altitude'])], 1)
+
+    pygame.display.flip()
 
     # CONTROLS
+
+    if pygame.time.get_ticks() > state['timer']:
+        state['timer'] += 1
+        if pygame.key.get_pressed()[273] and state['fuel'] > 0:
+            boost()
+        else:
+            gravity()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                hacer_cosas()
+
+    # END
+    if get_triangle_cords(100, 500 - state['altitude'])[1][1] > 490:
+        if abs(state['speed']) < 5:
+            print('SUCCESS')
         else:
-            # RESET DURATION
-
-
-class Vector:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def setPos(self, x, y):
-        self.x = x
-        self.y = y
-
-    def mag(self):
-        return math.hypot(self.x, self.y)
-
-    def getPos(self):
-        return [self.x, self.y]
-
-    def norm(self):
-        ans = [self.x/self.getMng(), self.y/self.getMng()]
-        ansV = Vector(ans[0], ans[1])
-        return ansV
-
-    def addV(self, v):
-        self.x += v.getPos()[0]
-        self.y += v.getPos()[1]
-
-    def subV(self, v):
-        self.x -= v.getPos()[0]
-        self.y -= v.getPos()[1]
-
-    def getAngle(self):
-        if(self.x == 0):
-            if(self.y > 0):
-                return math.pi/2
-            else:
-                return math.pi*3/2
-        ans = math.atan(self.y/self.x)
-        if (self.x < 0):
-            return ans+math.pi
-        return ans
-
-    def scalarMult(self, s):
-        self.x *= s
-        self.y *= s
-
-
-# Physics
-FUEL_CONSTANT = 1
-GRAVITY_CONSTANT = -1
-RESISTANCE_CONSTANT = -0.01
-DIRECTION = new Vector(0, 1)
-
-
-def update(state, fuel):
-    position = new Vector(init_x, init_y)
-    time = 0
-    state[3] += state[5]
-    state[5] += fuel*FUEL_CONSTANT+GRAVITY_CONSTANT
+            print('FAIL')
+        sys.exit()
