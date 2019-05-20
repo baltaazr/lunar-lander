9d6ecaefd1a07ade0fc11a8487969f2a674614d5
mrprokoala <zunigaruizb@gmail.com>
2019-05-20 12:59:20 +0800

    DONE

--- app.py
+++ app.py
@@ -1,81 +1,164 @@
-import pygame
+import pygame as pg
 import math
 import sys
+import numpy as np
+from operator import attrgetter
+
 # INITIAL STATE
-state = {'altitude': 490, 'fuel': 1000,  'speed': 0, 'timer': 0}
+state = {'timer': 0}
 
 step = 0.001
 
+number_of_specimen = 100
 
-def gravity():
-    # A(t) = 9.8
-    state['speed'] -= 9.8 * step
-    state['altitude'] += state['speed'] * step
+sequence_size = 300000
+
+generation_colors = ['white', 'blue', 'red',
+                     'yellow', 'green', 'purple', 'pink']
+
+current_generation = 0
 
 
-def boost():
-    # A(t) = 9.8 - 50 = 40.2
-    state['speed'] += 40.2 * step
-    state['altitude'] += state['speed'] * step
-    state['fuel'] -= 0.1
+class Specimen:
+    def __init__(self, sequence=None):
+        self.altitude = 490
+        self.fuel = 1000
+        self.speed = 0
+        self.color = generation_colors[(current_generation-1) % 7]
+        if sequence is None:
+            self.sequence = generate_sequence(sequence_size)
+        else:
+            self.sequence = sequence
+        self.test_result = None
+
+    def gravity(self):
+        # A(t) = 9.8
+        self.speed -= 9.8 * step
+        self.altitude += self.speed * step
+
+    def boost(self):
+        # A(t) = 9.8 - 50 = 40.2
+        self.speed += 40.2 * step
+        self.altitude += self.speed * step
+        self.fuel -= 0.1
+
+
+def generate_sequence(size):
+    p_false = np.random.random()
+    return np.random.choice(a=[False, True], size=size, p=[p_false, 1 - p_false])
+
+
+def generate_specimina(size=100):
+    specimina = []
+    for i in range(size):
+        specimina.append(Specimen())
+    return specimina
+
+
+def generate_next_specimina():
+    # GETTING TOP 10 BEST PERFORMING SPECIMEN
+    best_specimina = []
+    for i in range(10):
+        best_specimen = max(previous_specimina, key=attrgetter('test_result'))
+        best_specimina.append(best_specimen)
+        print(str(i + 1) + '. ' + str(best_specimen.test_result))
+        previous_specimina.remove(best_specimen)
+
+    # COPYING OVER 10 SPECIMEN FROM PREVIOUS GENERATION AT RANDOM
+    for i in range(10):
+        specimen = np.random.choice(previous_specimina)
+        previous_specimina.remove(specimen)
+        specimen.speed = 0
+        specimen.fuel = 1000
+        specimen.altitude = 490
+        specimina.append(specimen)
+
+    # GENERATING NEW SPECIMEN FROM TOP 10 BEST PERFORMING SPECIMEN OF LAST GENERATION
+    for i in range(90):
+        dad = np.random.choice(best_specimina)
+        mom = np.random.choice(best_specimina)
+        n = np.random.randint(sequence_size)
+        specimina.append(
+            Specimen(sequence=np.append(dad.sequence[:n], mom.sequence[n:])))
+
+    # MUTATION
+    noise = np.random.rand(sequence_size)
+    for i, n in enumerate(noise):
+        if n < 0.5:
+            specimina[99].sequence[i] = False
+        elif n > 0.90:
+            specimina[99].sequence[i] = True
+        specimina[99].color = 'orange'
+
+
+current_generation += 1
+
+specimina = generate_specimina(size=number_of_specimen)
+
+previous_specimina = []
 
 
 def get_triangle_cords(x, y, r=10):
     return [(x, y - r), (x + r*math.cos(math.pi*(5/4)), y - r*math.sin(math.pi*(5/4))), (x + r*math.cos(math.pi*(7/4)), y - r*math.sin(math.pi*(7/4)))]
 
 
-pygame.init()
+pg.init()
 
-pygame.font.init()
+pg.font.init()
 
-myfont = pygame.font.SysFont('Arial', 15)
+myfont = pg.font.SysFont('Arial', 15)
 
-size = width, height = 200, 500
+size = width, height = 500, 500
 black = 0, 0, 0
 
-screen = pygame.display.set_mode(size)
+screen = pg.display.set_mode(size)
 
 while(True):
-    # RUN GRAPHICS USING STATE
+    # GRAPHICS
     screen.fill(black)
 
-    altitude_text = myfont.render(
-        'altitude: ' + str(round(state['altitude'])), False, (255, 255, 255))
+    time_text = myfont.render(
+        'time: ' + str(round(state['timer']/1000)), False, (255, 255, 255))
 
-    fuel_text = myfont.render(
-        'fuel: ' + str(round(state['fuel'])), False, (255, 255, 255))
+    generation_text = myfont.render(
+        'current generation: ' + str(current_generation), False, (255, 255, 255))
 
-    speed_text = myfont.render(
-        'speed: ' + str(round(state['speed'])), False, (255, 255, 255))
+    screen.blit(time_text, (420, 470))
 
-    screen.blit(altitude_text, (0, 0))
-    screen.blit(fuel_text, (0, 20))
-    screen.blit(speed_text, (0, 40))
+    screen.blit(generation_text, (350, 450))
 
-    pygame.draw.line(screen, (255, 255, 255), (0, 490), (500, 490))
+    pg.draw.line(screen, (255, 255, 255), (0, 490), (500, 490))
 
-    pygame.draw.polygon(screen, (255, 255, 255), [(round(triangle[0]), round(triangle[1])) for triangle in get_triangle_cords(
-        100, 500 - state['altitude'])], 1)
+    for i, specimen in enumerate(specimina):
+        pg.draw.polygon(screen, pg.Color(specimen.color), [(round(triangle[0]), round(
+            triangle[1])) for triangle in get_triangle_cords(width*i/len(specimina), 500 - specimen.altitude)], 1)
 
-    pygame.display.flip()
+    pg.display.flip()
 
-    # CONTROLS
-
-    if pygame.time.get_ticks() > state['timer']:
-        state['timer'] += 1
-        if pygame.key.get_pressed()[273] and state['fuel'] > 0:
-            boost()
+    # MOVEMENT
+    state['timer'] += 1
+    for specimen in specimina:
+        if state['timer'] < sequence_size:
+            boost_bool = specimen.sequence[state['timer']] >= 0.5
         else:
-            gravity()
+            boost_bool = False
+        if boost_bool and specimen.fuel > 0:
+            specimen.boost()
+        else:
+            specimen.gravity()
+        if get_triangle_cords(100, 500 - specimen.altitude)[1][1] > 490:
+            specimen.test_result = specimen.speed
+            previous_specimina.append(specimen)
+            specimina.remove(specimen)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    if len(specimina) == 0:
+        current_generation += 1
+        print('GENERATING NEW GENERATION')
+        generate_next_specimina()
+        print('NEW GENERATION CREATED')
+        previous_specimina = []
+        state['timer'] = 0
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
             sys.exit()
-
-    # END
-    if get_triangle_cords(100, 500 - state['altitude'])[1][1] > 490:
-        if abs(state['speed']) < 5:
-            print('SUCCESS')
-        else:
-            print('FAIL')
-        sys.exit()
